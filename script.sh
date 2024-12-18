c7f948fbf70f6095faa79a2a0d1d26e996ceeb50
Mikhail Okhrimenko <mo@ps.kz>
2024-12-18 23:20:49 +0500

    script with error

--- script.sh
+++ script.sh
@@ -1,23 +1,17 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-if [[ $# -ne 3 ]]; then
-	echo "Usage: $0 FILE_SIZE_MB FILE_NAME_PREFIX FILE_COUNT"
-	exit 1
-fi
-
-FILE_SIZE_MB=$1
-PREFIX=$2
-FILE_COUNT=$3
-
-if ! [[ $FILE_SIZE_MB =~ ^[0-9]+$ ]] || ! [[ $FILE_COUNT =~ ^[0-9]+$ ]]; then
-    echo "Error: Both file size and file count must be positive integers."
+if [[ $# -ne 1 ]]; then
+    echo "Usage: $0 <directory_path>"
     exit 1
 fi
 
-for i in $(seq -w 1 $FILE_COUNT);do
-	FILENAME="${PREFIX}_${i}.dat"
-	dd if=/dev/zero of="$FILENAME" bs=1M count=$FILE_SIZE_MB status=none
-	echo "File $FILENAME created"
-done
+DIRECTRY=$1
 
-echo "Successfully created $FILE_COUNT files with prefix $PREFIX and size ${FILE_SIZE_MB}MB each."
+if [[ ! -d $DIRECTORY ]]; then
+    echo "Error: $DIRECTORY is not a valid directory."
+    exit 1
+fi
+
+FILE_COUNT=$(find "$DIRECTORY" | wc -l)
+
+echo "The directory '$DIRECTORY' contains $FILE_COUNT file(s)."
